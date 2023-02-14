9f5ae1d22e3d356f5e5434810f328861582b7a6a
Kirill <109174433+Kirillk1o@users.noreply.github.com>
2023-02-14 20:54:37 +0300

    0. Spring MVC [#6877]

--- src/main/java/ru/job4j/controller/IndexController.java
+++ src/main/java/ru/job4j/controller/IndexController.java
@@ -1,10 +1,11 @@
 package ru.job4j.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
-
+    @GetMapping("/index")
     public String getIndex() {
         return "index";
     }
