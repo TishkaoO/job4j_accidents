99e3f198a05f7681ce9aa6fd7e7ef2a9863f6e9e
Kirill <109174433+Kirillk1o@users.noreply.github.com>
2023-03-11 00:06:10 +0300

    0. Spring test [#6881 #399321]

--- src/test/java/ru/job4j/accident/controller/LoginControllerTest.java
+++ src/test/java/ru/job4j/accident/controller/LoginControllerTest.java
@@ -26,6 +26,6 @@ class LoginControllerTest {
        this.mockMvc.perform(get("/formLogin"))
                .andDo(print())
                .andExpect(status().isOk())
-               .andExpect(view().name("users/login"));
+               .andExpect(view().name("users/logi"));
     }
 }
