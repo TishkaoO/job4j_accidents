44634ba16b62e71a77ec281af059136c5ae90a8c
Kirill <109174433+Kirillk1o@users.noreply.github.com>
2023-03-11 19:33:41 +0300

    0. Spring test [#6881 #399321]

--- src/test/java/ru/job4j/accident/controller/AccidentControllerTest.java
+++ src/test/java/ru/job4j/accident/controller/AccidentControllerTest.java
@@ -19,7 +19,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 class AccidentControllerTest {
     @Autowired
-    private MockMvc mockMv;
+    private MockMvc mockMvc;
 
     @Test
     @WithMockUser
