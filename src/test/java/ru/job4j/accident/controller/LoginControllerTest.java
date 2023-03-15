40a41c3cd981d3a45f311694acfa94dcb57ec1cf
Kirill <109174433+Kirillk1o@users.noreply.github.com>
2023-03-15 23:05:02 +0300

    0. Spring test [#6881 #399321]

--- src/test/java/ru/job4j/accident/controller/LoginControllerTest.java
+++ src/test/java/ru/job4j/accident/controller/LoginControllerTest.java
@@ -14,6 +14,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
 @SpringBootTest(classes = MainApp.class)
 @AutoConfigureMockMvc
 class LoginControllerTest {
